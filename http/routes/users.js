a3dd5ed14c5a5737a177d4043364c7b63e45df7a
Martin Morães <martin.moraes@gmail.com>
2020-05-14 11:56:13 -0300

    Refatorado: utilizando app.route e utils

--- http/routes/users.js
+++ http/routes/users.js
@@ -1,18 +1,18 @@
 let NeDB = require('nedb')
-// let db = new NeDB({
-//     filename: 'users.db',
-//     autoload: true
-// })
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
 
 module.exports = (app) => {
-    app.get('/users', (req, res) => {
+
+    const route = app.route('/users')
+
+    route.get((req, res) => {
 
         db.find({}).sort({name:1}).exec((err, users) => {
             if (err) {
-                console.log(`error: ${err}`)
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res)
             } else {
                 res.statusCode = 200
                 res.setHeader('Content-Type', 'application/json')
@@ -31,14 +31,11 @@ module.exports = (app) => {
         })
     })
 
-    app.post('/users', (req, res) => {
+    route.post((req, res) => {
 
         db.insert(req.body, (err, user) => {
             if (err) {
-                console.log (`Erro: ${err}`)
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res)
             } else {
                 res.status(200).json(user)
             }
